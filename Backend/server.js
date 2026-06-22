e67a139f3135ee027e47c0b772704055f5ede141
Dhanujvardhan <dhanujvardhan0123@gmail.com>
2026-06-23 00:49:15 +0530

    Add Render port support

--- Backend/server.js
+++ Backend/server.js
@@ -27,34 +27,12 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
-// Products Database
-let products = [
-  { id: 1, name: "HP Victus", category: "Laptop", price: 65999 },
-  { id: 2, name: "Dell G15", category: "Laptop", price: 72999 },
-  { id: 3, name: "Lenovo LOQ", category: "Laptop", price: 69999 },
-  { id: 4, name: "Acer Nitro V", category: "Laptop", price: 67999 },
-  { id: 5, name: "MacBook Air M3", category: "Laptop", price: 109900 },
 
-  { id: 6, name: "Logitech G102", category: "Mouse", price: 1499 },
-  { id: 7, name: "Logitech G304", category: "Mouse", price: 2999 },
-
-  { id: 8, name: "Cosmic Byte CB-GK-16", category: "Keyboard", price: 1999 },
-  { id: 9, name: "Redragon K552", category: "Keyboard", price: 3499 },
-
-  { id: 10, name: "Sony WH-CH520", category: "Headphones", price: 4499 },
-  { id: 11, name: "Boat Rockerz 550", category: "Headphones", price: 1999 },
-
-  { id: 12, name: "JBL Go 3", category: "Speaker", price: 2999 },
-  { id: 13, name: "Boat Stone 350", category: "Speaker", price: 1799 },
-
-  { id: 14, name: "Razer Gigantus V2", category: "Mouse Pad", price: 999 },
-  { id: 15, name: "Realme Buds Air 6", category: "Earbuds", price: 2999 }
-];
 
 // Home Route
 app.get("/", (req, res) => {
@@ -83,18 +61,31 @@ app.get("/products",(req,res)=>{
 
 // GET Product By ID
 app.get("/products/:id", (req, res) => {
-  const id = parseInt(req.params.id);
 
-  const product = products.find(p => p.id === id);
+  const id = req.params.id;
 
-  if (!product) {
-    return res.status(404).json({
-      success: false,
-      message: "Product not found"
-    });
-  }
+  db.get(
+    "SELECT * FROM products WHERE id = ?",
+    [id],
 
-  res.status(200).json(product);
+    (err, row) => {
+
+      if (err) {
+        return res.status(500).json(err);
+      }
+
+      if (!row) {
+        return res.status(404).json({
+          success: false,
+          message: "Product not found"
+        });
+      }
+
+      res.status(200).json(row);
+
+    }
+  );
+
 });
 
 // POST New Product
